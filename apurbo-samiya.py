1841aa1ff0e3164e276a80add26e9311849fe908
apourbo <69768031+apourbo@users.noreply.github.com>
2022-08-12 01:05:26 +0600

    Main. File of samiya

---
+++ apurbo-samiya.py
@@ -0,0 +1,48 @@
+import pyfiglet
+from colorama  import Fore , Back , Style
+import time,os,sys
+
+def type(text):
+    for charecter in text:
+        sys.stdout.write(charecter)
+        sys.stdout.flush()
+        time.sleep(0.02)
+
+
+
+logo = pyfiglet.figlet_format("APURBO" , font = "graffiti")
+ig = Fore.GREEN + logo
+type(ig)
+type( Fore.BLUE + "A Tool From APURBO............\n")
+type(Fore.YELLOW + "Inspired By SAMIYA.........\n")
+type(Fore.GREEN + "IMPRESS YOUR GIRLFRIEND BY THIS TOOL\n")
+type("        1> TYPE WITH NUMBER\n")
+type("        2> TYPE WITHOUT NUMBER\n")
+
+
+ip = input("SELECT A NUMBER->    ")
+ipt = int(ip)
+
+if ipt == int(1):
+    text = input(Fore.RED + "What You Want To Type->    ")
+    num = input(Fore.BLUE + "Number Of Times You Want To Print->    ")
+    numi = int(num)
+    nm = numi + 1
+    for i in range(1 , nm):
+        print(Fore.GREEN + text , i)
+        
+        
+        
+        
+if ipt == int(2):
+    text = input(Fore.RED + "What You Want To Type->    ")
+    num = input(Fore.BLUE + "Number Of Times You Want To Print->    ")
+    numi = int(num)
+    for i in range(numi):
+        print(Fore.GREEN + text)
+    
+
+
+
+
+    
